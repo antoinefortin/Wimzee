f72aed63d0e0767072cf2dff59602be116a06f58
Antoine Fortin <afortin@Qube-4D.com>
2026-01-13 15:37:02 -0500

    Added STB for windows

--- setup/setup-dependencies-windows.ps1
+++ setup/setup-dependencies-windows.ps1
@@ -1,25 +1,32 @@
 # setup-deps.ps1
-Write-Host "Installing Speedlog" -ForegroundColor Green
+Write-Host "Installing dependencies..." -ForegroundColor Green
 
 if (!(Test-Path "vendor")) {
     New-Item -ItemType Directory -Path "vendor"
 }
 
+# spdlog
 if (!(Test-Path "vendor/spdlog")) {
-    Write-Host "Downloading speedlog"
+    Write-Host "Downloading spdlog..."
     git clone --depth 1 --branch v1.13.0 https://github.com/gabime/spdlog.git vendor/spdlog
 } else {
-    Write-Host "spdlog is already installed!"
+    Write-Host "spdlog already installed!"
 }
 
 # GLM
 if (!(Test-Path "vendor/glm")) {
-    Write-Host "Downloading  GLM..."
+    Write-Host "Downloading GLM..."
     git clone --depth 1 --branch 0.9.9.8 https://github.com/g-truc/glm.git vendor/glm
 } else {
-    Write-Host "GLM déjà installé!"
+    Write-Host "GLM already installed!"
 }
 
-
+# stb (stb_image)
+if (!(Test-Path "vendor/stb")) {
+    Write-Host "Downloading stb..."
+    git clone --depth 1 https://github.com/nothings/stb.git vendor/stb
+} else {
+    Write-Host "stb already installed!"
+}
 
 Write-Host "Terminé!" -ForegroundColor Green
